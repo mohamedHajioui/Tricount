dd08871cfe468152b58df5e1a392862c9978c8fa
Aly Samir <2208alsamirmahmoudib@student.epfc.eu>
2025-04-23 11:23:17 +0200

    feat(get_my_tricount): une premiere version fonctionnelle

--- backend/03_tables_and_data.sql
+++ backend/03_tables_and_data.sql
@@ -7,8 +7,11 @@ create table tricount
     id  serial primary key,
     title  varchar(256) not null,
     description  varchar(512),
-    creator varchar(256) not null,
+    creator int not null,
     participant  integer[],
     date_hour timestamp default current_timestamp 
 );
+
+
+insert into tricount(title, description, creator, participant) values ('japon','voyage epfc informatique',2,'{3,4,9}')
 
--- backend/06_endpoints.sql
+++ backend/06_endpoints.sql
@@ -2,16 +2,14 @@ create or replace function save_tricount(
     save_id int,
     save_title text,
     save_description text,
-    save_creator int,
     save_participants integer[]) returns void  as 
     $$
     begin
-        perform auth.check_logged();
-        if save_id = 0 then
-            
-            insert into tricount(title, description, participant,creator) values (save_title,save_description,save_participants,save_creator);
+        perform auth.check_logged(); --on check la connection de l'utilisateur
+        if save_id = 0 then --create 
+            insert into tricount(title, description, participant,creator) values (save_title,save_description,save_participants,auth.id());
         end if;
-        if save_id > 0 then
+        if save_id > 0 then --update
             update tricount set title = save_title , description = save_description , participant = save_participants
                             where id = save_id;
         end if;
@@ -21,8 +19,20 @@ $$language plpgsql security definer;
 
 grant execute on function save_tricount to anon;
 
+create or replace function get_my_tricounts() 
+    returns setof tricount as
+    $$
+    begin
+        perform auth.check_logged(); --on check la connection de l'utilisateur
+        return query select tricount.*
+                    from tricount
+                    where tricount.creator = auth.id();
+    end;
+    $$language plpgsql security definer ;
 
-    
+select * from get_my_tricounts();
+
+grant execute on function get_my_tricounts() to anon;
 
     
     
