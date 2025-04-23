b0b3b4b9614607107b66b0c871349432ca49e85e
Hamza Stitou <hamzastitou11s@hotmail.com>
2025-04-23 09:57:07 +0200

    feat(backend) : get_user_data

--- backend/06_endpoints.sql
+++ backend/06_endpoints.sql
@@ -18,5 +18,15 @@ create or replace function save_tricount(
 $$language plpgsql security definer; 
 
 grant execute on function save_tricount to anon;
-
+create or replace function get_user_data()
+    returns setof users as
+$$
+begin
+    perform auth.check_logged();
+    return query select *
+                 from users
+                 where users.email = auth.email();
+end;
+$$ language plpgsql security definer;
+grant execute on function get_user_data() to authenticated;
 
