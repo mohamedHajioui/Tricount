bc275abd206a0d5a062b214639c1adabe8fc0f1a
Aly Samir <2208alsamirmahmoudib@student.epfc.eu>
2025-04-23 09:17:25 +0200

    feat(tricount): sql

--- backend/06_endpoints.sql
+++ backend/06_endpoints.sql
@@ -1,15 +1,19 @@
 create or replace function save_tricount(
-    id int,
+    save_id int,
     save_title text,
     save_description text,
-    save_participant integer[]) returns int  as 
+    save_participants integer[]) returns void  as 
     $$
     begin 
-        if id = 0 then
-            insert into tricount(title, description, participant) values (save_title,save_description,save_participant);
+        if save_id = 0 then
+            insert into tricount(title, description, participant) values (save_title,save_description,save_participants);
         end if;
-        
-        
+        if save_id > 0 then
+            update tricount set title = save_title and description = save_description and participant = save_participants
+                            where id = save_id;
+        end if;
     end;
     
-$$
+$$language plpgsql;
+
+grant execute on function save_tricount to anon;
