3dcfba78b3aafdbb483f56aba8ec0a5771f2e8fd
Aly Samir <2208alsamirmahmoudib@student.epfc.eu>
2025-05-01 10:28:13 +0200

    fix: merge

--- backend/03_tables_and_data.sql
+++ backend/03_tables_and_data.sql
@@ -24,10 +24,11 @@ create table depense(
     repartition jsonb not null
 );
 
-drop table if exists participation;
-create table participation(
-    user_id int not null,
-    tricount_id int not null
-)
+
+create table participation (
+    user_id integer not null references users(id) on delete cascade,
+    tricount_id integer not null references tricount(id) on delete cascade,
+    primary key (user_id, tricount_id)
+);
 
 
--- backend/06_endpoints.sql
+++ backend/06_endpoints.sql
@@ -2,14 +2,16 @@ create or replace function save_tricount(
     save_id int,
     save_title text,
     save_description text,
+    save_creator int,
     save_participants integer[]) returns void  as 
     $$
     begin
-        perform auth.check_logged(); --on check la connection de l'utilisateur
-        if save_id = 0 then --create 
-            insert into tricount(title, description, participant,creator) values (save_title,save_description,save_participants,auth.id());
+        perform auth.check_logged();
+        if save_id = 0 then
+            
+            insert into tricount(title, description, participant,creator) values (save_title,save_description,save_participants,save_creator);
         end if;
-        if save_id > 0 then --update
+        if save_id > 0 then
             update tricount set title = save_title , description = save_description , participant = save_participants
                             where id = save_id;
         end if;
@@ -19,24 +21,6 @@ $$language plpgsql security definer;
 
 grant execute on function save_tricount to anon;
 
-create or replace function get_my_tricounts() 
-    returns setof tricount as
-    $$
-    begin
-        perform auth.check_logged(); --on check la connection de l'utilisateur
-        return query select tricount.*
-                    from tricount
-                    where tricount.creator = auth.id();
-    end;
-    $$language plpgsql security definer ;
-
-select * from get_my_tricounts();
-
-grant execute on function get_my_tricounts() to anon;
-
-    
-    
-    
 create or replace function get_user_data()
     returns setof users as
 $$
@@ -68,21 +52,33 @@ $$
     end;
 $$ language plpgsql security definer;
 grant execute on function check_email_available(text, integer) to anon;
-create or replace function check_full_name_available(full_name text,user_id integer default 0)
-returns boolean as 
-    $$
-    begin 
-        if user_id =0 then 
-            return not exists(
-                select 1 from users where users.full_name = check_full_name_available.full_name
-                );
-        else
-            return not exists(
-                select 1 from users 
-                where users.full_name = check_full_name_available.full_name
-                and users.id <> check_full_name_available.user_id
-                );
-        end if;
-    end;
-    $$ language plpgsql security definer ;
-grant execute on function check_full_name_available(text,integer) to anon;
+
+
+create or replace function get_all_users()
+returns setof users as
+$$
+  begin 
+      return query
+        select *
+        from users
+        order by full_name;
+      
+end;
+$$ language plpgsql security definer;
+grant execute on function get_all_users() to authenticated;
+
+
+create or replace function delete_tricount(tricount_id text)
+    returns void as
+$$
+    declare 
+        v_tricount_id text := tricount_id;
+begin
+    --verifie que l'user est connecté
+    perform auth.check_logged();
+
+    delete from depense where depense.tricount_id = v_tricount_id;
+    delete from part
+end;
+
+$$ language plpgsql;
