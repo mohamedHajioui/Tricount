2465b12db3b8fbd298c9a50c2a81b1c8e2d22ccf
Hamza Stitou <hamzastitou11s@hotmail.com>
2025-05-02 21:22:11 +0200

    feat(backend): creations des endpoints

--- backend/03_tables_and_data.sql
+++ backend/03_tables_and_data.sql
@@ -7,27 +7,28 @@ create table tricount
     id  serial primary key,
     title  varchar(256) not null,
     description  varchar(512),
-    creator int not null,
+    creator int not null references users(id),
     participant  integer[],
-    date_hour timestamp default current_timestamp 
+    date_hour timestamp default current_timestamp
 );
 
 drop table if exists operateur;
 drop table if exists depense;
 create table depense(
-    id serial primary key,
-    tricount_id int,
-    title varchar(256) not null ,
-    amount double precision not null,
-    operation_date timestamp default null,
-    initiator int not null,
-    repartition jsonb not null
+                        id serial primary key,
+                        tricount_id int not null references tricount(id) on delete restrict,
+                        title varchar(256) not null,
+                        amount double precision not null,
+                        operation_date timestamp not null default current_DATE,  -- date de la dépense
+                        created_at timestamp not null default current_timestamp,      -- date de création
+                        initiator int not null,
+                        repartition jsonb not null
 );
 
 create table participation (
-    user_id integer not null references users(id) on delete cascade,
-    tricount_id integer not null references tricount(id) on delete cascade,
-    primary key (user_id, tricount_id)
+                               user_id integer not null references users(id) on delete cascade,
+                               tricount_id integer not null references tricount(id) on delete cascade,
+                               primary key (user_id, tricount_id)
 );
 
 
